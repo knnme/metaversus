5fc3d01b302f7500a80996c5cf9d90a82289b70e
Kennedy Melo <kennedymelolp@gmail.com>
2022-11-26 11:41:57 -0300

    final commit

--- constants/index.js
+++ constants/index.js
@@ -30,6 +30,7 @@ export const startingFeatures = [
   'Find a world that suits you and you want to enter',
   'Enter the world by reading basmalah to be safe',
   'No need to beat around the bush, just stay on the gas and have fun',
+
 ];
 
 export const newFeatures = [
